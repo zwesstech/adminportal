7d6c3b1c80e65e5e82a7355168b918ce3d49c34b
Sizwe <nkosiczwe@gmail.com>
2020-08-12 08:45:26 +0200

    add AdminPortal login function.

--- src/main/java/com/adminportal/service/UserService.java
+++ src/main/java/com/adminportal/service/UserService.java
@@ -7,13 +7,6 @@ import com.adminportal.domain.security.PasswordResetToken;
 import com.adminportal.domain.security.UserRole;
 
 public interface UserService {
-	 PasswordResetToken getPasswordResetToken(final String token); 
-		
-		void createPasswordResetTokenForUser(final User user, final String token);
-		
-		User findByUsername(String username);
-		
-		User findByEmail (String email);
 		
 		User createUser(User user,Set<UserRole> userRoles) throws Exception; 
 		
